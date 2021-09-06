49f85324e4267e064d9f49072a96d1bf9007a979
vlarysc <74836578+vlarysc@users.noreply.github.com>
2021-09-06 13:16:44 -0300

    updates

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const answer_bEl = document.getElementById("answerB");
 const answer_cEl = document.getElementById("answerC");
 const answer_dEl = document.getElementById("answerD");
 const answer_eEl = document.getElementById("answerE");
-const correctAnswerEl = document.getElementById("correct-answer")
+const chosenAnswer = document.getElementById("chosen-answer");
+const correctAnswerEl = document.getElementById("correct-answer");
+const explanationEl = document.getElementById("explanation");
+var score = 0;
 var currentQuestion = 0;
 let correctAnswer = "";
 
-
- axios.get("http://localhost:3000/questions").then(function (data) {
+axios.get("http://localhost:3000/questions").then(function (data) {
   console.log(data.data.map((data) => data.question));
 
   const question = data.data[currentQuestion].question;
@@ -19,7 +21,6 @@ let correctAnswer = "";
   const answerD = data.data[currentQuestion].answerD;
   const answerE = data.data[currentQuestion].answerE;
 
-
   correctAnswer = data.data[currentQuestion].correct_answer;
   questionEl.innerHTML = question;
   answer_aEl.innerHTML = `a) ${answerA}`;
@@ -27,117 +28,195 @@ let correctAnswer = "";
   answer_cEl.innerHTML = `c) ${answerC}`;
   answer_dEl.innerHTML = `d) ${answerD}`;
   answer_eEl.innerHTML = `e) ${answerE}`;
-   correctAnswerEl.innerHTML =  teste(); 
-   function teste() {
-     
-     switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
-       case 'A':
-        return `A resposta correta é a letra : <br> <br> A) ${answerA}`;
-         break;
-       case 'B':
-         return `A resposta correta é a letra : <br> <br> B) ${answerB}`;
-         break;
-       case 'C':
-         return  `A resposta correta é a letra : <br> <br> C) ${answerC}`;
-         break;
-       case 'D':
-         return `A resposta correta é a letra : <br> <br> D) ${answerD}`;
-         break;
-       case 'E':
-         return  `A resposta correta é a letra : <br> <br> E) ${answerE}`;
-         
-     }
-   }
-});
 
+  correctAnswerEl.innerHTML = correctAnswerValidate();
+  function correctAnswerValidate() {
+    switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
+      case "A":
+        return `A resposta correta é a letra : <br> <br> A) ${answerA}`;
+        break;
+      case "B":
+        return `A resposta correta é a letra : <br> <br> B) ${answerB}`;
+        break;
+      case "C":
+        return `A resposta correta é a letra : <br> <br> C) ${answerC}`;
+        break;
+      case "D":
+        return `A resposta correta é a letra : <br> <br> D) ${answerD}`;
+        break;
+      case "E":
+        return `A resposta correta é a letra : <br> <br> E) ${answerE}`;
+    }
+  }
+});
 
 function validateAnswer() {
-  const questionsEl = document.getElementById('questions')
-  questionsEl.classList.add("visible")
-  const answersEl = document.getElementById('answers')
-  answersEl.classList.remove("visible")
+  const questionsEl = document.getElementById("questions");
+  questionsEl.classList.add("visible");
+  const answersEl = document.getElementById("answers");
+  answersEl.classList.remove("visible");
+  const answerChosed = this.id;
+  const scoreEl = document.getElementById("score");
 
-  
- 
- if(this.id == correctAnswer) {
-   /* currentQuestion += 1 */
-   axios.get("http://localhost:3000/questions").then(function (data) {
-  console.log(data.data.map((data) => data.question));
+  if (this.id == correctAnswer) {
+    score += 1;
 
-  const question = data.data[currentQuestion].question;
-  const answerA = data.data[currentQuestion].answerA;
-  const answerB = data.data[currentQuestion].answerB;
-  const answerC = data.data[currentQuestion].answerC;
-  const answerD = data.data[currentQuestion].answerD;
-  const answerE = data.data[currentQuestion].answerE;
+    axios.get("http://localhost:3000/questions").then(function (data) {
+      console.log(data.data.map((data) => data.question));
 
-  correctAnswer = data.data[currentQuestion].correct_answer;
-  questionEl.innerHTML = question;
-  answer_aEl.innerHTML = `a) ${answerA}`;
-  answer_bEl.innerHTML = `b) ${answerB}`;
-  answer_cEl.innerHTML = `c) ${answerC}`;
-  answer_dEl.innerHTML = `d) ${answerD}`;
-  answer_eEl.innerHTML = `e) ${answerE}`;
-  correctAnswerEl.innerHTML =  teste(); 
-   function teste() {
-     
-     switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
-       case 'A':
-        return `A resposta correta é a letra : <br> <br> A) ${answerA}`;
-         break;
-       case 'B':
-         return `A resposta correta é a letra : <br> <br> B) ${answerB}`;
-         break;
-       case 'C':
-         return  `A resposta correta é a letra : <br> <br> C) ${answerC}`;
-         break;
-       case 'D':
-         return `A resposta correta é a letra : <br> <br> D) ${answerD}`;
-         break;
-       case 'E':
-         return  `A resposta correta é a letra : <br> <br> E) ${answerE}`;
-         
-     }
-   }
-});
-   
-} else {
-  console.log("Errou")
-}
+      const question = data.data[currentQuestion].question;
+      const answerA = data.data[currentQuestion].answerA;
+      const answerB = data.data[currentQuestion].answerB;
+      const answerC = data.data[currentQuestion].answerC;
+      const answerD = data.data[currentQuestion].answerD;
+      const answerE = data.data[currentQuestion].answerE;
+      const explanation = data.data[currentQuestion].explanation;
+
+      explanationEl.innerHTML = explanation;
+      correctAnswer = data.data[currentQuestion].correct_answer;
+      questionEl.innerHTML = question;
+      answer_aEl.innerHTML = `a) ${answerA}`;
+      answer_bEl.innerHTML = `b) ${answerB}`;
+      answer_cEl.innerHTML = `c) ${answerC}`;
+      answer_dEl.innerHTML = `d) ${answerD}`;
+      answer_eEl.innerHTML = `e) ${answerE}`;
+      scoreEl.innerHTML = `Pontuação: <strong>${score} </strong>`;
+      chosenAnswer.innerHTML = answerChosedValidate();
+      correctAnswerEl.innerHTML = correctAnswerValidate();
+
+      function correctAnswerValidate() {
+        switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
+          case "A":
+            return `A resposta correta é a letra : <br> <br> A) ${answerA}`;
+            break;
+          case "B":
+            return `A resposta correta é a letra : <br> <br> B) ${answerB}`;
+            break;
+          case "C":
+            return `A resposta correta é a letra : <br> <br> C) ${answerC}`;
+            break;
+          case "D":
+            return `A resposta correta é a letra : <br> <br> D) ${answerD}`;
+            break;
+          case "E":
+            return `A resposta correta é a letra : <br> <br> E) ${answerE}`;
+        }
+      }
+
+      function answerChosedValidate() {
+        switch (answerChosed.split("r").pop().toLocaleUpperCase()) {
+          case "A":
+            return `Você escolheu a: <br> <br> A) ${answerA}`;
+            break;
+          case "B":
+            return `Você escolheu a: <br> <br> B) ${answerB}`;
+            break;
+          case "C":
+            return `Você escolheu a: <br> <br> C) ${answerC}`;
+            break;
+          case "D":
+            return `Você escolheu a: <br> <br> D) ${answerD}`;
+            break;
+          case "E":
+            return `Você escolheu a: <br> <br> E) ${answerE}`;
+        }
+      }
+    });
+  } else {
+    axios.get("http://localhost:3000/questions").then(function (data) {
+      console.log(data.data.map((data) => data.question));
+
+      const question = data.data[currentQuestion].question;
+      const answerA = data.data[currentQuestion].answerA;
+      const answerB = data.data[currentQuestion].answerB;
+      const answerC = data.data[currentQuestion].answerC;
+      const answerD = data.data[currentQuestion].answerD;
+      const answerE = data.data[currentQuestion].answerE;
+      const explanation = data.data[currentQuestion].explanation;
+
+      explanationEl.innerHTML = explanation;
+      correctAnswer = data.data[currentQuestion].correct_answer;
+      questionEl.innerHTML = question;
+      answer_aEl.innerHTML = `a) ${answerA}`;
+      answer_bEl.innerHTML = `b) ${answerB}`;
+      answer_cEl.innerHTML = `c) ${answerC}`;
+      answer_dEl.innerHTML = `d) ${answerD}`;
+      answer_eEl.innerHTML = `e) ${answerE}`;
+      scoreEl.innerHTML = `Pontuação: <strong>${score} </strong>`;
+      chosenAnswer.innerHTML = answerChosedValidate();
+      correctAnswerEl.innerHTML = correctAnswerValidate();
+
+      function correctAnswerValidate() {
+        switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
+          case "A":
+            return `A resposta correta é a letra : <br> <br> A) ${answerA}`;
+            break;
+          case "B":
+            return `A resposta correta é a letra : <br> <br> B) ${answerB}`;
+            break;
+          case "C":
+            return `A resposta correta é a letra : <br> <br> C) ${answerC}`;
+            break;
+          case "D":
+            return `A resposta correta é a letra : <br> <br> D) ${answerD}`;
+            break;
+          case "E":
+            return `A resposta correta é a letra : <br> <br> E) ${answerE}`;
+        }
+      }
+
+      function answerChosedValidate() {
+        switch (answerChosed.split("r").pop().toLocaleUpperCase()) {
+          case "A":
+            return `Você escolheu a: <br> <br> A) ${answerA}`;
+            break;
+          case "B":
+            return `Você escolheu a: <br> <br> B) ${answerB}`;
+            break;
+          case "C":
+            return `Você escolheu a: <br> <br> C) ${answerC}`;
+            break;
+          case "D":
+            return `Você escolheu a: <br> <br> D) ${answerD}`;
+            break;
+          case "E":
+            return `Você escolheu a: <br> <br> E) ${answerE}`;
+        }
+      }
+    });
+  }
 }
 
 function newQuestion() {
-  currentQuestion += 1
-   axios.get("http://localhost:3000/questions").then(function (data) {
-  console.log(data.data.map((data) => data.question));
+  currentQuestion += 1;
+  axios.get("http://localhost:3000/questions").then(function (data) {
+    console.log(data.data.map((data) => data.question));
 
-  const question = data.data[currentQuestion].question;
-  const answerA = data.data[currentQuestion].answerA;
-  const answerB = data.data[currentQuestion].answerB;
-  const answerC = data.data[currentQuestion].answerC;
-  const answerD = data.data[currentQuestion].answerD;
-  const answerE = data.data[currentQuestion].answerE;
+    const question = data.data[currentQuestion].question;
+    const answerA = data.data[currentQuestion].answerA;
+    const answerB = data.data[currentQuestion].answerB;
+    const answerC = data.data[currentQuestion].answerC;
+    const answerD = data.data[currentQuestion].answerD;
+    const answerE = data.data[currentQuestion].answerE;
 
-  correctAnswer = data.data[currentQuestion].correct_answer;
-  questionEl.innerHTML = question;
-  answer_aEl.innerHTML = `a) ${answerA}`;
-  answer_bEl.innerHTML = `b) ${answerB}`;
-  answer_cEl.innerHTML = `c) ${answerC}`;
-  answer_dEl.innerHTML = `d) ${answerD}`;
-  answer_eEl.innerHTML = `e) ${answerE}`;
- 
-   
-});
-  const answersEl = document.getElementById('answers')
-  const questionsEl = document.getElementById('questions')
+    correctAnswer = data.data[currentQuestion].correct_answer;
+    questionEl.innerHTML = question;
+    answer_aEl.innerHTML = `a) ${answerA}`;
+    answer_bEl.innerHTML = `b) ${answerB}`;
+    answer_cEl.innerHTML = `c) ${answerC}`;
+    answer_dEl.innerHTML = `d) ${answerD}`;
+    answer_eEl.innerHTML = `e) ${answerE}`;
+  });
+  const answersEl = document.getElementById("answers");
+  const questionsEl = document.getElementById("questions");
 
-  questionsEl.classList.remove("visible")
+  questionsEl.classList.remove("visible");
 
-  answersEl.classList.add("visible")
+  answersEl.classList.add("visible");
 }
 
-const answers = document.querySelectorAll('.text-answer')
+const answers = document.querySelectorAll(".text-answer");
 
-answers.forEach(element => {
-  element.addEventListener("click",validateAnswer)
+answers.forEach((element) => {
+  element.addEventListener("click", validateAnswer);
 });
