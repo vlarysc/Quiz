7198716d8322f363d4b0903a7c3243447e099efd
vlarysc <74836578+vlarysc@users.noreply.github.com>
2021-09-06 20:27:52 -0300

    Co-authored-by: Hugo da Silva Gomes <hdsg0807@gmail.com>

--- script.js
+++ script.js
@@ -5,223 +5,88 @@ const answer_cEl = document.getElementById("answerC");
 const answer_dEl = document.getElementById("answerD");
 const answer_eEl = document.getElementById("answerE");
 const chosenAnswer = document.getElementById("chosen-answer");
-const correctAnswerEl = document.getElementById("correct-answer");
+const correct_answerEl = document.getElementById("correct-answer");
 const explanationEl = document.getElementById("explanation");
-var score = 0;
-var currentQuestion = 0;
-let correctAnswer = "";
+const scoreEl = document.getElementById("score");
+let score = 0;
+let currentQuestion = 0;
+let correct_answer = "";
+let questions = [];
+let question = "";
+let answerA = "";
+let answerB = "";
+let answerC = "";
+let answerD = "";
+let answerE = "";
+let explanation = "";
 
-axios.get("https://my-json-server.typicode.com/vlarysc/fakeAPI-Quiz/questions").then(function (data) {
-  console.log(data.data.map((data) => data.question));
+axios
+  .get("https://my-json-server.typicode.com/vlarysc/fakeAPI-Quiz/questions")
+  .then(function (data) {
+    questions = data;
+    dataControl();
+    dataControlInnerHtml()
+  });
 
-  const question = data.data[currentQuestion].question;
-  const answerA = data.data[currentQuestion].answerA;
-  const answerB = data.data[currentQuestion].answerB;
-  const answerC = data.data[currentQuestion].answerC;
-  const answerD = data.data[currentQuestion].answerD;
-  const answerE = data.data[currentQuestion].answerE;
+function dataControl() {
+  ({
+    question,
+    answerA,
+    answerB,
+    answerC,
+    answerD,
+    answerE,
+    correct_answer,
+    explanation,
+  } = questions.data[currentQuestion]);
+}
 
-  correctAnswer = data.data[currentQuestion].correct_answer;
-  questionEl.innerHTML = question;
-  answer_aEl.innerHTML = `a) ${answerA}`;
-  answer_bEl.innerHTML = `b) ${answerB}`;
-  answer_cEl.innerHTML = `c) ${answerC}`;
-  answer_dEl.innerHTML = `d) ${answerD}`;
-  answer_eEl.innerHTML = `e) ${answerE}`;
+function dataControlInnerHtml() {
+    questionEl.innerHTML = question;
+    answer_aEl.innerHTML = `a) ${answerA}`;
+    answer_bEl.innerHTML = `b) ${answerB}`;
+    answer_cEl.innerHTML = `c) ${answerC}`;
+    answer_dEl.innerHTML = `d) ${answerD}`;
+    answer_eEl.innerHTML = `e) ${answerE}`;
+}
 
-  correctAnswerEl.innerHTML = correctAnswerValidate();
-  function correctAnswerValidate() {
-    switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
-      case "A":
-        return `Resposta correta: ${answerA}`;
-        break;
-      case "B":
-        return `Resposta correta: ${answerB}`;
-        break;
-      case "C":
-        return `Resposta correta: ${answerC}`;
-        break;
-      case "D":
-        return `Resposta correta: ${answerD}`;
-        break;
-      case "E":
-        return `Resposta correta: ${answerE}`;
-    }
-  }
-});
+function updateScore(score) {
+  score = score;
+  scoreEl.innerHTML = `Pontuação: <strong>${score} </strong>`;
+}
 
 function validateAnswer() {
   const questionsEl = document.getElementById("questions");
-  questionsEl.classList.add("visible");
   const answersEl = document.getElementById("answers");
+
+  questionsEl.classList.add("visible");
   answersEl.classList.remove("visible");
-  const answerChosed = this.id;
-  const scoreEl = document.getElementById("score");
 
-  if (this.id == correctAnswer) {
-    score += 1;
-
-    axios.get("https://my-json-server.typicode.com/vlarysc/fakeAPI-Quiz/questions").then(function (data) {
-      console.log(data.data.map((data) => data.question));
-
-      const question = data.data[currentQuestion].question;
-      const answerA = data.data[currentQuestion].answerA;
-      const answerB = data.data[currentQuestion].answerB;
-      const answerC = data.data[currentQuestion].answerC;
-      const answerD = data.data[currentQuestion].answerD;
-      const answerE = data.data[currentQuestion].answerE;
-      const explanation = data.data[currentQuestion].explanation;
-
-      explanationEl.innerHTML = explanation;
-      correctAnswer = data.data[currentQuestion].correct_answer;
-      questionEl.innerHTML = question;
-      answer_aEl.innerHTML = `a) ${answerA}`;
-      answer_bEl.innerHTML = `b) ${answerB}`;
-      answer_cEl.innerHTML = `c) ${answerC}`;
-      answer_dEl.innerHTML = `d) ${answerD}`;
-      answer_eEl.innerHTML = `e) ${answerE}`;
-      scoreEl.innerHTML = `Pontuação: <strong>${score} </strong>`;
-      chosenAnswer.innerHTML = answerChosedValidate();
-      correctAnswerEl.innerHTML = correctAnswerValidate();
-
-      function correctAnswerValidate() {
-        switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
-          case "A":
-            return `Resposta correta: ${answerA}`;
-            break;
-          case "B":
-            return `Resposta correta: ${answerB}`;
-            break;
-          case "C":
-            return `Resposta correta: ${answerC}`;
-            break;
-          case "D":
-            return `Resposta correta: ${answerD}`;
-            break;
-          case "E":
-            return `Resposta correta: ${answerE}`;
-        }
-      }
-
-      function answerChosedValidate() {
-        switch (answerChosed.split("r").pop().toLocaleUpperCase()) {
-          case "A":
-            return `Você escolheu: ${answerA}`;
-            break;
-          case "B":
-            return `Você escolheu: ${answerB}`;
-            break;
-          case "C":
-            return `Você escolheu: ${answerC}`;
-            break;
-          case "D":
-            return `Você escolheu: ${answerD}`;
-            break;
-          case "E":
-            return `Você escolheu: ${answerE}`;
-        }
-      }
-    });
-  } else {
-    axios.get("https://my-json-server.typicode.com/vlarysc/fakeAPI-Quiz/questions").then(function (data) {
-      console.log(data.data.map((data) => data.question));
-
-      const question = data.data[currentQuestion].question;
-      const answerA = data.data[currentQuestion].answerA;
-      const answerB = data.data[currentQuestion].answerB;
-      const answerC = data.data[currentQuestion].answerC;
-      const answerD = data.data[currentQuestion].answerD;
-      const answerE = data.data[currentQuestion].answerE;
-      const explanation = data.data[currentQuestion].explanation;
-
-      explanationEl.innerHTML = explanation;
-      correctAnswer = data.data[currentQuestion].correct_answer;
-      questionEl.innerHTML = question;
-      answer_aEl.innerHTML = `a) ${answerA}`;
-      answer_bEl.innerHTML = `b) ${answerB}`;
-      answer_cEl.innerHTML = `c) ${answerC}`;
-      answer_dEl.innerHTML = `d) ${answerD}`;
-      answer_eEl.innerHTML = `e) ${answerE}`;
-      scoreEl.innerHTML = `Pontuação: <strong>${score} </strong>`;
-      chosenAnswer.innerHTML = answerChosedValidate();
-      correctAnswerEl.innerHTML = correctAnswerValidate();
-
-      function correctAnswerValidate() {
-        switch (correctAnswer.split("r").pop().toLocaleUpperCase()) {
-          case "A":
-            return `Resposta correta: ${answerA}`;
-            break;
-          case "B":
-            return `Resposta correta: ${answerB}`;
-            break;
-          case "C":
-            return `Resposta correta: ${answerC}`;
-            break;
-          case "D":
-            return `Resposta correta: ${answerD}`;
-            break;
-          case "E":
-            return `Resposta correta: ${answerE}`;
-        }
-      }
-
-      function answerChosedValidate() {
-        switch (answerChosed.split("r").pop().toLocaleUpperCase()) {
-          case "A":
-            return `Você escolheu: ${answerA}`;
-            break;
-          case "B":
-            return `Você escolheu: ${answerB}`;
-            break;
-          case "C":
-            return `Você escolheu: ${answerC}`;
-            break;
-          case "D":
-            return `Você escolheu: ${answerD}`;
-            break;
-          case "E":
-            return `Você escolheu: ${answerE}`;
-        }
-      }
-    });
-  }
+  this.id == correct_answer && updateScore(++score);
+  explanationEl.innerHTML = explanation;
+  correct_answerEl.innerHTML = questions.data[currentQuestion][correct_answer];
+  chosenAnswer.innerHTML = `Você escolheu: ${this.textContent}`;
 }
 
 function newQuestion() {
-  if(currentQuestion <= 8) {
+  if (currentQuestion <= 8) {
     currentQuestion += 1;
-    axios.get("https://my-json-server.typicode.com/vlarysc/fakeAPI-Quiz/questions").then(function (data) {
-      console.log(data.data.map((data) => data.question));
-  
-      const question = data.data[currentQuestion].question;
-      const answerA = data.data[currentQuestion].answerA;
-      const answerB = data.data[currentQuestion].answerB;
-      const answerC = data.data[currentQuestion].answerC;
-      const answerD = data.data[currentQuestion].answerD;
-      const answerE = data.data[currentQuestion].answerE;
-  
-      correctAnswer = data.data[currentQuestion].correct_answer;
-      questionEl.innerHTML = question;
-      answer_aEl.innerHTML = `a) ${answerA}`;
-      answer_bEl.innerHTML = `b) ${answerB}`;
-      answer_cEl.innerHTML = `c) ${answerC}`;
-      answer_dEl.innerHTML = `d) ${answerD}`;
-      answer_eEl.innerHTML = `e) ${answerE}`;
-    });
+    dataControl();
+    dataControlInnerHtml()
+
     const answersEl = document.getElementById("answers");
     const questionsEl = document.getElementById("questions");
-  
+
     questionsEl.classList.remove("visible");
     answersEl.classList.add("visible");
-  }else {
+  } else {
     const quizEl = document.getElementById("quiz");
     const congratulationsEl = document.getElementById("congratulations");
 
     quizEl.classList.add("visible");
-    congratulationsEl.innerHTML = `Parabéns você concluiu o quiz`
+    congratulationsEl.innerHTML = `Parabéns você concluiu o quiz`;
     congratulationsEl.classList.remove("visible");
   }
-  
 }
 
 const answers = document.querySelectorAll(".text-answer");
